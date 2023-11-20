b31867d5bf65a53ef8a564a756c41b11e7d473d8
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-11-20 17:25:23 +0100

    002_CustomMobs : Removed dmg indicator feature as it is now an existing standalone plugin, found at its own branch

--- src/main/java/io/github/stealingdapenta/idletd/service/custommob/CustomMob.java
+++ src/main/java/io/github/stealingdapenta/idletd/service/custommob/CustomMob.java
@@ -14,30 +14,19 @@ public abstract class CustomMob {
 
     protected EntityType entityType;
 
-    //     Armor bonus of an Entity.
-    protected double GENERIC_ARMOR = 1.0;
-    //    Armor durability bonus of an Entity.
-    protected double GENERIC_ATTACK_DAMAGE = 1.0;
-    //     Attack damage of an Entity.
-    protected double GENERIC_ATTACK_KNOCKBACK = 1.0;
-    //     Attack knockback of an Entity.
-    protected double GENERIC_ARMOR_TOUGHNESS = 1.0;
-    //     Attack speed of an Entity.
-    protected double GENERIC_ATTACK_SPEED = 1.0;
-    //     Flying speed of an Entity.
-    protected double GENERIC_FLYING_SPEED = 1.0;
-    //    Range at which an Entity will follow others.
-    protected double GENERIC_FOLLOW_RANGE = 1.0;
-    //    Resistance of an Entity to knockback.
-    protected double GENERIC_KNOCKBACK_RESISTANCE = 1.0;
-    //    Luck bonus of an Entity.
-    protected double GENERIC_LUCK = 1.0;
-    //    Maximum absorption of an Entity.
-    protected double GENERIC_MAX_ABSORPTION = 1.0;
-    //    Maximum health of an Entity.
-    protected double GENERIC_MAX_HEALTH = 1.0;
-    //    Movement speed of an Entity.
-    protected double GENERIC_MOVEMENT_SPEED = 1.0;
+    protected double GENERIC_ARMOR = 1.0; //     Armor bonus of an Entity.
+    protected double GENERIC_ATTACK_DAMAGE = 1.0; //    Armor durability bonus of an Entity.
+    protected double GENERIC_ATTACK_KNOCKBACK = 1.0;    //     Attack damage of an Entity.
+    protected double GENERIC_ARMOR_TOUGHNESS = 1.0;    //     Attack knockback of an Entity.
+    protected double GENERIC_ATTACK_SPEED = 1.0;    //     Attack speed of an Entity.
+    protected double GENERIC_FLYING_SPEED = 1.0;    //     Flying speed of an Entity.
+    protected double GENERIC_FOLLOW_RANGE = 1.0;    //    Range at which an Entity will follow others.
+    protected double GENERIC_KNOCKBACK_RESISTANCE = 1.0;    //    Resistance of an Entity to knockback.
+    protected double GENERIC_LUCK = 1.0;    //    Luck bonus of an Entity.
+    protected double GENERIC_MAX_ABSORPTION = 1.0;    //    Maximum absorption of an Entity.
+    protected double GENERIC_MAX_HEALTH = 1.0;    //    Maximum health of an Entity.
+    protected double GENERIC_MOVEMENT_SPEED = 1.0;    //    Movement speed of an Entity.
+
 
     public Mob summon(Location location) {
         LivingEntity livingEntity = (LivingEntity) location.getWorld().spawnEntity(location, this.getEntityType()); // todo summon "this"
