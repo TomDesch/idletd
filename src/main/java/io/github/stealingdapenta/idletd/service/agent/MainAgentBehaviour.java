703ce404dafdb4fe463d0a05bcd4866b28004977
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-11-27 12:56:27 +0100

    002_CustomMobs : WIP POC Skin managing + WIP POC Agent NPC

---
+++ src/main/java/io/github/stealingdapenta/idletd/service/agent/MainAgentBehaviour.java
@@ -0,0 +1,35 @@
+package io.github.stealingdapenta.idletd.service.agent;
+
+import net.citizensnpcs.api.ai.tree.Behavior;
+import net.citizensnpcs.api.ai.tree.BehaviorStatus;
+
+public class MainAgentBehaviour implements Behavior {
+
+
+    @Override
+    public void reset() {
+//        state = null;
+        // this method can be called at any time if another goal is selected
+    }
+
+    @Override
+    public BehaviorStatus run() {
+        return BehaviorStatus.FAILURE;
+//        if(!npcIsCool()) {
+//            return BehaviorStatus.FAILURE;
+//        } else if (npcIsAwesome()) {
+//            return BehaviorStatus.SUCCESS;
+//        } else if (npcNeedsCool()) {
+//            new AccumulateCoolBehavior().run(); // easily run other behavior inline
+//            return BehaviorStatus.RUNNING;
+//        }
+    }
+
+    @Override
+    public boolean shouldExecute() {
+//        if (npcIsCool()) {
+//            return true;
+//        }
+        return false;
+    }
+}
---
+++ src/main/java/io/github/stealingdapenta/idletd/service/agent/MainAgentGoal.java
@@ -0,0 +1,49 @@
+package io.github.stealingdapenta.idletd.service.agent;
+
+import io.github.stealingdapenta.idletd.plot.Plot;
+import io.github.stealingdapenta.idletd.service.custommob.CustomMobHandler;
+import net.citizensnpcs.api.ai.Goal;
+import net.citizensnpcs.api.ai.GoalSelector;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class MainAgentGoal implements Goal {
+
+    private final Player player;
+    private final Plot plot;
+    private final Location centerLocation;
+    private NPC npc;
+
+    public MainAgentGoal(Player player, Plot plot) {
+        this.player = player;
+        this.plot = plot;
+        this.customMobHandler = CustomMobHandler.getInstance();
+
+        this.centerLocation = calculatePosition();
+
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void run(GoalSelector goalSelector) {
+        // the Main Agent is staticly places, and won't ever need to run
+    }
+
+    @Override
+    public boolean shouldExecute(GoalSelector goalSelector) {
+        return true;
+    }
+
+
+    private Location calculatePosition() {
+        Location spawnLocation = plot.getPlayerSpawnPoint();
+        spawnLocation.add(0d, -10d, -8d);
+        spawnLocation.setYaw(180); // Facing north
+        spawnLocation.setPitch(0);
+        return spawnLocation;
+    }
+}
