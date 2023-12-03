4a54362dfce3b16fb7bade4af783bfce71220ecc
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-12-03 11:16:00 +0100

    002_CustomMobs : Update wave logic to be one task for all active games + finalized TowerDefenseManager + General clean up

--- src/main/java/io/github/stealingdapenta/idletd/service/command/TowerDefenseCommand.java
+++ src/main/java/io/github/stealingdapenta/idletd/service/command/TowerDefenseCommand.java
@@ -5,6 +5,7 @@ import io.github.stealingdapenta.idletd.idleplayer.IdlePlayerService;
 import io.github.stealingdapenta.idletd.plot.Plot;
 import io.github.stealingdapenta.idletd.plot.PlotService;
 import io.github.stealingdapenta.idletd.towerdefense.TowerDefense;
+import io.github.stealingdapenta.idletd.towerdefense.TowerDefenseManager;
 import io.github.stealingdapenta.idletd.towerdefense.TowerDefenseService;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
@@ -25,6 +26,7 @@ public class TowerDefenseCommand implements CommandExecutor {
     private final PlotService plotService;
     private final TowerDefenseService towerDefenseService;
     private final IdlePlayerService idlePlayerService;
+    private final TowerDefenseManager towerDefenseManager;
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -34,10 +36,17 @@ public class TowerDefenseCommand implements CommandExecutor {
         if (Objects.isNull(idlePlayer)) {
             logger.severe(player.getName() + " doesn't have a linked IdlePlayer.");
             player.sendMessage(Component.text(NO_IDLE_PLAYER));
-            return false;
+            return true;
         }
 
-        TowerDefense towerDefense = towerDefenseService.findTowerDefense(idlePlayer);
+        TowerDefense towerDefense = towerDefenseManager.getActiveTDGame(idlePlayer);
+
+        if (Objects.nonNull(towerDefense)) {
+            player.sendMessage("You already have an active game.");
+            return true;
+        }
+
+        towerDefense = towerDefenseService.findTowerDefense(idlePlayer);
 
         if (Objects.isNull(towerDefense)) {
             Plot plot = plotService.findPlot(player);
@@ -45,7 +54,7 @@ public class TowerDefenseCommand implements CommandExecutor {
             if (Objects.isNull(plot)) {
                 logger.info(player.getName() + " tried to start a TD game without even owning a plot yet.");
                 player.sendMessage(Component.text(NO_PLOT));
-                return false;
+                return true;
             }
 
 
@@ -54,15 +63,14 @@ public class TowerDefenseCommand implements CommandExecutor {
                                        .playerUUID(idlePlayer.getPlayerUUID())
                                        .stageLevel(1)
                                        .build();
+            towerDefenseService.saveTowerDefense(towerDefense);
         }
 
-        towerDefenseService.startWave(towerDefense);
+        towerDefenseManager.activateTDGame(towerDefense);
 
-        // todo save the game regularly
+
         // todo calculate AFK income (idle)
-
         // todo add economy, reward per kill, bank value
-
         // todo voor afk gold income, misschien "highest income op een round" + "duur round" saven en dan percentage daarvan per minuut uitbetalen met zelfde verhouding
         return true;
     }
--- src/main/java/io/github/stealingdapenta/idletd/towerdefense/TowerDefenseManager.java
+++ src/main/java/io/github/stealingdapenta/idletd/towerdefense/TowerDefenseManager.java
@@ -1,26 +1,78 @@
 package io.github.stealingdapenta.idletd.towerdefense;
 
+import io.github.stealingdapenta.idletd.Idletd;
 import io.github.stealingdapenta.idletd.idleplayer.IdlePlayer;
+import io.github.stealingdapenta.idletd.idleplayer.IdlePlayerService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+
+import static io.github.stealingdapenta.idletd.service.utils.Time.ONE_SECOND_IN_TICKS;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class TowerDefenseManager {
     private static final Set<TowerDefense> activeTDGames = new HashSet<>();
+    private static BukkitTask activeGameManager;
+    private final IdlePlayerService idlePlayerService;
+    private final TowerDefenseService towerDefenseService;
 
+    // todo
+    // store active games upon activating (future: setting auto activate upon login? )
+    //
+    // upon log out: deactivate game & kill all the mobs AFTER (so no lvl increase or gold income)
+    // Save the game state
+
+    // todo further: summon NPC for target
+    // todo further: fix "endless mode" on waves without crashing
+    // then turn POC wave mobs into real deal with levels on mobs etc
+    // todo update in the future to have one task that loops over all active tower defense games
+    // todo Make waves generate infinite new ones; e.g. by taking re-looping over the fixed enum & multiplying the mob levels (using modulo) <= CRUCIAL
+
+    public void initializeActiveGameManager() {
+        if (Objects.isNull(activeGameManager)) {
+            activeGameManager = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    handleActiveGames();
+                }
+            }.runTaskTimer(Idletd.getInstance(), ONE_SECOND_IN_TICKS, ONE_SECOND_IN_TICKS / 2);
+        }
+    }
+
+    public void activateTDGame(TowerDefense towerDefense) {
+        addActiveTDGame(towerDefense);
+        towerDefenseService.startWave(towerDefense);
+    }
+
+    private void handleActiveGames() {
+        activeTDGames.forEach(this::doWaveCycle);
+    }
+
+    private void doWaveCycle(TowerDefense towerDefense) {
+        towerDefenseService.handleWaveEnd(towerDefense);
+        towerDefense.updateLivingMobs();
+    }
 
     public boolean addActiveTDGame(TowerDefense towerDefense) {
         return activeTDGames.add(towerDefense);
     }
 
     public boolean deactivateTDGame(TowerDefense towerDefense) {
+        towerDefense.setWaveActive(false);
+        killAllMobs(towerDefense);
         return activeTDGames.remove(towerDefense);
+    }
+
+    private void killAllMobs(TowerDefense towerDefense) {
+        towerDefense.getLivingMobs().forEach(mob -> mob.getMob().remove());
     }
 
     public boolean hasActiveTDGame(IdlePlayer idlePlayer) {
@@ -33,6 +85,4 @@ public class TowerDefenseManager {
                             .findFirst()
                             .orElse(null);
     }
-
-
 }
