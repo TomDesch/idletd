5b4055cd7c4853cda562cec8e9744d1adc43575f
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-12-03 15:27:48 +0100

    005_Skins : Skin repository & Skin manager & service + more population in skin table

---
+++ src/main/java/io/github/stealingdapenta/idletd/skin/Skin.java
@@ -0,0 +1,33 @@
+package io.github.stealingdapenta.idletd.skin;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
+
+@Builder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Skin {
+    private final long id;
+    private final String name;
+    private final String description;
+    private final String dataToken;
+    private final String signatureToken;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Skin skin = (Skin) o;
+        return id == skin.id || Objects.equals(dataToken, skin.dataToken) || Objects.equals(signatureToken, skin.signatureToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dataToken, signatureToken);
+    }
+}
