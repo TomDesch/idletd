c5058d4903d257c5312c3d8c7b0c0f6e7a5544fe
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-12-03 15:40:34 +0100

    006_POC_Main_Agent : Added main Agent location to Plot + Fixed facing direction for player.

--- src/main/java/io/github/stealingdapenta/idletd/plot/Plot.java
+++ src/main/java/io/github/stealingdapenta/idletd/plot/Plot.java
@@ -19,6 +19,7 @@ public class Plot {
     private static final Vector RELATIVE_TOWER_COORDINATES = new Vector(200, 80, 50);
     private static final Vector RELATIVE_PLAYER_SPAWN_COORDINATES = new Vector(200, 90, 43);
     private static final Vector RELATIVE_MOB_SPAWN_COORDINATES = new Vector(200, 81, -13);
+    private static final Vector RELATIVE_MAIN_AGENT_COORDINATES = new Vector(200, 71, -21);
 
     private long id;
     private int startX;
@@ -38,7 +39,17 @@ public class Plot {
     }
 
     public Location getPlayerSpawnPoint() {
-        return calculateLocation(RELATIVE_PLAYER_SPAWN_COORDINATES);
+        Location location = calculateLocation(RELATIVE_PLAYER_SPAWN_COORDINATES);
+        location.setYaw(180); // north
+        location.setPitch(0);
+        return location;
+    }
+
+    public Location getMainAgentLocation() {
+        Location location = calculateLocation(RELATIVE_MAIN_AGENT_COORDINATES);
+        location.setYaw(180); // north
+        location.setPitch(0);
+        return location;
     }
 
     private Location calculateLocation(Vector offset) {
