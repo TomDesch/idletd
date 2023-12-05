f5c61e84ff39f7dfc41cd04001fce1fa8339ce16
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-12-05 22:21:54 +0100

    008_mob_target_and_level_system : Replaced skeleton arrows with particle trail (with colliding checks!)

--- src/main/java/io/github/stealingdapenta/idletd/listener/DamageListener.java
+++ src/main/java/io/github/stealingdapenta/idletd/listener/DamageListener.java
@@ -1,10 +1,9 @@
 package io.github.stealingdapenta.idletd.listener;
 
-import io.github.stealingdapenta.idletd.Idletd;
 import io.github.stealingdapenta.idletd.custommob.CustomMobHandler;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -17,9 +16,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 
+import java.util.List;
 import java.util.Objects;
 
 @RequiredArgsConstructor
@@ -32,72 +31,6 @@ public class DamageListener implements Listener {
 
     private final CustomMobHandler customMobHandler;
 
-    private Vector addRandomnessToArrowVector(Vector vector, double accuracy) {
-        double x = vector.getX() + (Math.random() * 2 - 1) * accuracy;
-        double z = vector.getZ() + (Math.random() * 2 - 1) * accuracy;
-
-        return new Vector(x, vector.getY(), z).normalize();
-    }
-
-
-    private boolean isMob(Entity entity) {
-        return entity instanceof Mob;
-    }
-
-    private boolean isProjectile(Entity entity) {
-        return entity instanceof Projectile;
-    }
-
-    private boolean isCustomMobDamage(Entity source, Entity target) {
-        return isMob(source) && customMobHandler.isCustomMob((Mob) source) && isMob(target) && customMobHandler.isCustomMob((Mob) target);
-    }
-
-    private boolean isCustomArrow(Projectile projectile) {
-        return projectile.getPersistentDataContainer().has(getCustomArrowNSK(), PersistentDataType.BYTE);
-    }
-
-    private void cancelDamageEvent(EntityDamageEvent event) {
-        event.setDamage(0);
-        event.setCancelled(true);
-    }
-
-    private void cancelCustomMobArrowDamage(EntityDamageByEntityEvent event, Projectile projectile) {
-        cancelDamageEvent(event);
-        // Set the arrow's velocity to keep it flying towards its initial destination
-        Vector originalVelocity = projectile.getVelocity();
-        projectile.setVelocity(originalVelocity);
-    }
-
-    private NamespacedKey getCustomArrowNSK() {
-        return new NamespacedKey(Idletd.getInstance(), "customArrow");
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onSkeletonShoot(EntityShootBowEvent event) {
-        LivingEntity shooter = event.getEntity();
-        Arrow originalArrow = (Arrow) event.getProjectile();
-
-        if (!customMobHandler.isCustomMob(shooter)) return;
-
-        event.setCancelled(true);
-
-        Arrow newArrow = shooter.launchProjectile(Arrow.class);
-
-        Entity targetEntity = ((Skeleton) shooter).getTarget();
-        if (Objects.isNull(targetEntity)) return;
-
-        Location targetLocation = targetEntity.getLocation();
-        Location arrowLocation = originalArrow.getLocation();
-
-        Vector targetDirection = targetLocation.toVector().subtract(arrowLocation.toVector()).normalize();
-
-        double accuracy = 0.2; // Adjust this value for accuracy, 0 being 100% accurate
-        Vector randomizedDirection = addRandomnessToArrowVector(targetDirection, accuracy);
-        newArrow.setVelocity(randomizedDirection.multiply(newArrow.getVelocity().length()));
-
-        // Add a persistent tag to the arrow to identify it as a custom arrow
-        newArrow.getPersistentDataContainer().set(getCustomArrowNSK(), PersistentDataType.BYTE, (byte) 1);
-    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void cancelUnwantedDamageEvents(EntityDamageByEntityEvent event) {
@@ -111,9 +44,120 @@ public class DamageListener implements Listener {
 
         if (isProjectile(source)) {
             Projectile projectile = (Projectile) source;
-            if (isCustomArrow(projectile) && (isMob(target) && customMobHandler.isCustomMob((Mob) target))) {
-                cancelCustomMobArrowDamage(event, projectile);
+
+            if ((isMob(target) && customMobHandler.isCustomMob((Mob) target))) {
+                if (projectile.getShooter() instanceof LivingEntity shooter && customMobHandler.isCustomMob(shooter)) {
+                    cancelDamageEvent(event);
+                }
+
             }
         }
+    }
+
+    private boolean isMob(Entity entity) {
+        return entity instanceof Mob;
+    }
+
+    private boolean isProjectile(Entity entity) {
+        return entity instanceof Projectile;
+    }
+
+    private boolean isCustomMobDamage(Entity source, Entity target) {
+        return isMob(source) && customMobHandler.isCustomMob((Mob) source) && isMob(target) && customMobHandler.isCustomMob((Mob) target);
+    }
+
+    private void cancelDamageEvent(EntityDamageEvent event) {
+        event.setDamage(0);
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onSkeletonShoot(EntityShootBowEvent event) {
+        LivingEntity shooter = event.getEntity();
+        Arrow originalArrow = (Arrow) event.getProjectile();
+
+        if (!customMobHandler.isCustomMob(shooter)) return;
+
+        event.setCancelled(true);
+
+        Entity targetEntity = ((Skeleton) shooter).getTarget();
+        if (Objects.isNull(targetEntity)) return;
+
+        Location targetLocation = targetEntity.getLocation();
+        targetLocation = targetLocation.add(0, 1.5, 0); // adjusting to height
+        Location arrowLocation = originalArrow.getLocation();
+
+        Vector targetDirection = targetLocation.toVector().subtract(arrowLocation.toVector()).normalize();
+
+        double accuracy = 0.2; // Adjust this value for accuracy, 0 being 100% accurate
+        Vector randomizedDirection = addRandomnessToArrowVector(targetDirection, accuracy);
+        spawnParticleTrail(arrowLocation, randomizedDirection, 0.1);
+    }
+
+    private Vector addRandomnessToArrowVector(Vector vector, double accuracy) {
+        double x = vector.getX() + (Math.random() * 2 - 1) * accuracy;
+        double z = vector.getZ() + (Math.random() * 2 - 1) * accuracy;
+
+        return new Vector(x, vector.getY(), z).normalize();
+    }
+
+    private void spawnParticleTrail(Location location, Vector direction, double offset) {
+        boolean collisionOccurred = false;
+        int i = 0;
+        double gravity = -0.05; // Adjust this value for gravity effect
+
+        while (!collisionOccurred) {
+            double x = offset * i * direction.getX();
+            double y = offset * i * direction.getY() + 0.5 * gravity * i * i; // Add arc effect
+            double z = offset * i * direction.getZ();
+
+            Location particleLocation = location.clone().add(x, y, z);
+
+            if (particleCollidesWithEntity(particleLocation)) {
+                List<LivingEntity> collidedEntities = getCollidedEntities(particleLocation);
+                if (collidedEntities.stream().allMatch(customMobHandler::isCustomMob)) {
+                    location.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, particleLocation, 1);
+                } else {
+                    animateEntityImpactParticles(particleLocation);
+                    collisionOccurred = true;
+                }
+            } else if (particleCollidesWithSolidBlock(particleLocation)) {
+                animateGroundImpactParticles(particleLocation);
+                collisionOccurred = true;
+            } else {
+                location.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, particleLocation, 1);
+            }
+
+            i++;
+
+            if (i > 500) { // failsafe break
+                collisionOccurred = true;
+            }
+        }
+    }
+
+
+    private boolean particleCollidesWithEntity(Location location) {
+        return location.getWorld().getNearbyEntities(location, 0.5, 0.5, 0.5).stream().anyMatch(LivingEntity.class::isInstance);
+    }
+
+    private boolean particleCollidesWithSolidBlock(Location location) {
+        return !location.getBlock().isPassable();
+    }
+
+    private List<LivingEntity> getCollidedEntities(Location location) {
+        return location.getWorld().getNearbyEntities(location, 0.5, 0.5, 0.5)
+                       .stream()
+                       .filter(LivingEntity.class::isInstance)
+                       .map(LivingEntity.class::cast)
+                       .toList();
+    }
+
+    private void animateEntityImpactParticles(Location location) {
+        location.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, location, 3, 0, 0, 0, 0);
+    }
+
+    private void animateGroundImpactParticles(Location location) {
+        location.getWorld().spawnParticle(Particle.SMOKE_NORMAL, location, 15, 0, 0, 0, 0.1);
     }
 }
