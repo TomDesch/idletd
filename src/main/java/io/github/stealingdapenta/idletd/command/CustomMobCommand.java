a03cc8a916fb66293bd7f539e51dcf80049668b8
StealingDaPenta <tomdescheemaeker@hotmail.com>
2023-12-25 16:00:27 +0100

    008_mob_target_and_level_system : Added a remover function for customly spawned monsters by command

--- src/main/java/io/github/stealingdapenta/idletd/command/CustomMobCommand.java
+++ src/main/java/io/github/stealingdapenta/idletd/command/CustomMobCommand.java
@@ -1,29 +1,49 @@
 package io.github.stealingdapenta.idletd.command;
 
+import io.github.stealingdapenta.idletd.Idletd;
 import io.github.stealingdapenta.idletd.custommob.mobtypes.CustomMob;
 import io.github.stealingdapenta.idletd.custommob.mobtypes.SkeletonMob;
 import io.github.stealingdapenta.idletd.custommob.mobtypes.ZombieMob;
+import io.github.stealingdapenta.idletd.plot.Plot;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.BlockIterator;
 
 @RequiredArgsConstructor
 public class CustomMobCommand implements CommandExecutor {
+
+    private static final String CUSTOM_SUMMON = "customsummon";
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         Player sourcePlayer = (Player) commandSender;
 
-        if (args.length < 2) {
+        if (args.length < 1) {
             return false;
         }
 
+        if (args.length < 2) {
+            if ("killall".equals(args[0])) {
+                sourcePlayer.sendMessage("Killing all custom summoned monsters nearby you.");
+                sourcePlayer.getLocation()
+                            .getNearbyEntities(20, 20, 20)
+                            .stream()
+                            .filter(this::isCustomSummoned)
+                            .forEach(org.bukkit.entity.Entity::remove);
+                return true;
+            }
+        }
+
         String typeString = args[0];
-        String levelString = args[1];
+        String levelString = args.length > 1 ? args[1] : "1";
         int level;
 
         try {
@@ -35,30 +55,48 @@ public class CustomMobCommand implements CommandExecutor {
 
         int amount = 1;
         try {
-            amount = Integer.parseInt(args[2]);
+            if (args.length > 2) {
+                amount = Integer.parseInt(args[2]);
+            }
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
             sourcePlayer.sendMessage("No/wrong amount specified, defaulting 1.");
         }
 
-        CustomMob customMob;
+        Plot fictionalPlot = new Plot(0, 0, 0, sourcePlayer.getUniqueId());
+
         for (int i = 0; i < amount; i++) {
-            switch (typeString.toLowerCase()) {
-                case "zombie":
-                    customMob = new ZombieMob(null, level);
-                case "skeleton":
-                    customMob = new SkeletonMob(null, level);
-                default:
-                    customMob = new ZombieMob(null, level);
-            }
+            CustomMob customMob = switch (typeString.toLowerCase()) {
+                case "zombie" -> new ZombieMob(fictionalPlot, level);
+                case "skeleton" -> new SkeletonMob(fictionalPlot, level);
+                default -> new ZombieMob(fictionalPlot, level);
+            };
 
             customMob.summon(getLocationOnTopOfBlock(sourcePlayer));
-            customMob.getMob().setTarget(sourcePlayer);
+            customMob.getMob()
+                     .setTarget(sourcePlayer);
+            customMob.getMob()
+                     .getPersistentDataContainer()
+                     .set(getCustomSummonKey(), PersistentDataType.BOOLEAN, true);
         }
 
         return true;
     }
 
+
+    private NamespacedKey getCustomSummonKey() {
+        return new NamespacedKey(Idletd.getInstance(), CUSTOM_SUMMON);
+    }
+
+    public boolean isCustomSummoned(Entity entity) {
+        return Boolean.TRUE.equals(entity.getPersistentDataContainer()
+                                         .get(getCustomSummonKey(), PersistentDataType.BOOLEAN));
+    }
+
     public static Location getLocationOnTopOfBlock(Player player) {
+        // todo: fix the spawn position
+        // fix agents duplication (npc) todo
+        // Scale movement speed with a max lvl todo
+
         BlockIterator blockIterator = new BlockIterator(player, 5); // Adjust the range as needed
 
         while (blockIterator.hasNext()) {
