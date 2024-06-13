24118afc4ecf1aba65ae423fbd36b92f3817496d
StealingDaPenta <tomdescheemaeker@hotmail.com>
2024-06-13 16:40:21 +0200

    008_mob_target_and_level_system : Removed sensitive info

--- src/main/java/io/github/stealingdapenta/idletd/database/DatabaseConfig.java
+++ src/main/java/io/github/stealingdapenta/idletd/database/DatabaseConfig.java
@@ -1,9 +1,11 @@
 package io.github.stealingdapenta.idletd.database;
 
 public class DatabaseConfig {
-    public static final String DATABASE_URL = "jdbc:mysql://162.222.196.126/mc287946";
-    public static final String DATABASE_USERNAME = "mc287946";
-    public static final String DATABASE_PASSWORD = "414c082eca";
+
+    public static final String DATABASE_URL = System.getenv("DB_URL");
+    ;
+    public static final String DATABASE_USERNAME = System.getenv("DB_USERNAME");
+    public static final String DATABASE_PASSWORD = System.getenv("DB_PASSWORD");
     // admin url : http://162.222.196.126/phpmyadmin
 
     private DatabaseConfig() {
